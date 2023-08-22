618df2afa979fb1e9ae96877a98e4c67db55cc62
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-08-22 11:38:34 -0400

    Added basic folders to the project

---
+++ Utilities/fpsCounter.py
@@ -0,0 +1,9 @@
+import pygame
+import time
+import math
+
+
+class FpsCounter:
+    """This class is designed to be used as a decorator. Add this as a decorator and there should be an fps counter in the top right of the screen"""
+    def __init__(self):
+        pass
