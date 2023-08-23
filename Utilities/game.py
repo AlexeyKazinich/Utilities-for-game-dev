4e380ff791db5cd1baed4ba93e6bbb7e6536474a
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-08-23 00:33:53 -0400

    added a basic game implementation

--- Utilities/game.py
+++ Utilities/game.py
@@ -2,14 +2,32 @@ import pygame
 
 class Game:
     def __init__(self):
-        
-        self.run()
+        pygame.init()
+        self.screen_width = 800
+        self.screen_height = 600
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.running = True
+        self.clock = pygame.time.Clock()
      
     def run(self):
-        pass
+        self.draw()
+        self.logic_checks()
     
     def draw(self):
         pass
     
     def logic_checks(self):
-        pass
+        while self.running:
+            self.fps_counter.count_fps()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+
+                    
+            self.draw()
+            self.logic()
+            
+            # Update the display
+            pygame.display.flip()
+            self.clock.tick(60)
