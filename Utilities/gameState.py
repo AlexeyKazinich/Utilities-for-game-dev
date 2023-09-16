fb9fb974a21fcc41c94f3fb75af2560788d57fea
AlexeyKazinich <alexey.kazinich@gmail.com>
2023-09-15 23:52:09 -0400

    moved game.py into gameState

--- Utilities/gameState.py
+++ Utilities/gameState.py
@@ -54,3 +54,35 @@ class GameScreen(Screen):
     
     def logic_checks(self):
         super().logic_checks()
+        
+        
+class Game:
+    """this is a default implementation of a game, to use this import Game then call the run function"""
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen_size = (800,600)
+        self.window = pygame.display.set_mode((self.screen_size[0],self.screen_size[1]))
+        self.running = True
+        self.clock = pygame.time.Clock()
+        
+    def run(self) -> None:
+        """initial call to get the game to run"""
+        while self.running:
+            self.logic_checks()
+            self.draw()
+            self.clock.tick(60)
+            
+    def draw(self) -> None:
+        """draws everything to the screen"""
+        self.window.fill((255,255,255))
+        pygame.display.flip()
+        
+    def logic_checks(self) -> None:
+        """runs all the logic for the game"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+        
+    
--- Utilities/utils.py
+++ Utilities/utils.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+import time
 
 
 def load_image(path: str = "",colorkey: tuple = (0,0,0)) -> pygame.Surface:
@@ -33,3 +33,40 @@ class Animation:
     
     def img(self) -> pygame.Surface:
         return self.images[int(self.frame / self.img_duration)]
+    
+    
+class FpsCounter:
+    """This class is designed only to show the current FPS, it does not allow to limit the framerate of the game"""
+    def __init__(self,window: pygame.display,font_size: int = 12,background_color: tuple = (0,0,0,0))-> None:
+        pygame.font.init()
+        
+        self.cSec = 0
+        self.cFrame = 0
+        self.FPS= 0
+        self.deltatime = 0
+        self.font_fps = pygame.font.Font(None,font_size)
+        self.color_fps = pygame.Color(0,255,0)
+        self.area = window.get_rect()
+        self.window = window
+        self.tickrate = 100
+    
+    def count_fps(self)-> None:
+        """run this inside the game loop"""
+        if self.cSec == time.strftime("%S"):
+            self.cFrame +=1
+        else:
+            self.FPS = self.cFrame
+            self.cFrame = 0
+            self.cSec = time.strftime("%S")
+            if self.FPS > 0:
+                self.deltatime = 1 / self.FPS
+                
+                
+    def draw(self)-> None:
+        """call this to draw the frame counter"""
+        #background rectangle
+        
+        
+        #text
+        self.FPS_counter_surface = self.font_fps.render(str(math.floor(self.FPS))+"FPS",True,self.color_fps)
+        self.window.blit(self.FPS_counter_surface,(self.area.width-self.FPS_counter_surface.get_width(),0))
